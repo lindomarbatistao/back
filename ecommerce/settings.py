ccc88aa9efce6dfd2604751e4b31b6b9241b9da5
Lindomar <lindomarbatistao@gmail.com>
2024-05-10 08:28:37 -0300

    10-05-2024 v1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3v$s*u7()y727%=3i0r(_%@*ew0=fe)lvh9_9q&1iq=+^jgoj)'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lintelecom.pythonanywhere.com', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 
 MIDDLEWARE_CLASSES = [
